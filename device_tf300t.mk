8000445993178e3ccde3647f0406575b89e03be2
Menno van Grinsven <scanno71@gmail.com>
2013-11-05 21:07:51 +0100

    TF300T:
    Change-Id: I50d845822fab869d0e05bdd35557952a81222054

--- device_tf300t.mk
+++ device_tf300t.mk
@@ -52,6 +52,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     $(LOCAL_PATH)/asusdec/com.cyanogenmod.asusdec.xml:system/etc/permissions/com.cyanogenmod.asusdec.xml
+$(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \	
+$(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 # Build characteristics setting 
 PRODUCT_CHARACTERISTICS := tablet
